387a0b5cc4bb88f753680e705098e1354f66f110
Clarisse Otmani <clarisseotm@gmail.com>
2024-10-17 16:07:55 +0200

    modification de la class account pour coller au cahier des charges

--- src/main/java/fr/afpa/orm/entities/Account.java
+++ src/main/java/fr/afpa/orm/entities/Account.java
@@ -1,33 +1,19 @@
 package fr.afpa.orm.entities;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 
 /**
  * Classe représentant le compte bancaire d'un utilisateur
- * 
+ * <p>
  * TODO faire de cette classe une entité
  * Plus d'informations sur les entités -> https://gayerie.dev/epsi-b3-orm/javaee_orm/jpa_entites.html
  * Attention de bien choisir les types en fonction de ceux du script SQL.
  */
 @Entity
-@Table(name = "T_account")
+@Table(name = "account")
 public class Account {
     /**
      * Identifiant unique du compte
@@ -36,25 +22,26 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String firstName;
-    private String lastName;
-    private  String email;
-    private LocalDate birthDate;
     private LocalDateTime creationTime;
     private BigDecimal balance;
     /**
      * TODO ajout d'une association de type @ManyToOne : plusieurs comptes différents peuvent être associés à la même personne
-     * 
+     * <p>
      * Tutoriel présentant l'utilisation d'une telle association : https://koor.fr/Java/TutorialJEE/jee_jpa_many_to_one.wp
      */
-//    @ManyToOne
-//    @JoinColumn(name = "client_id")
-//    private Client customer;
+    @JoinColumn(name = "id_client")
+    @ManyToOne(targetEntity = Client.class)
+    private Client client;
 
     /*
      *
      */
+    public Account(Client client) {
+        this.client = client;
+    }
+
     public Account() {
+
     }
 
     public Long getId() {
@@ -63,38 +50,6 @@ public class Account {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
     }
 
     public LocalDateTime getCreationTime() {
@@ -113,12 +68,13 @@ public class Account {
         this.balance = balance;
     }
 
-//    public Client getClient {
-//        return client;
-//    }
-//
-//    public void setPerson(Client customer) {
-//        this.client = customer;
-//    }
-//}
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
 }
+

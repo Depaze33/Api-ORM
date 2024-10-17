5ce0552f719ba333a059ed2d0dd5ff5469bbb911
Clarisse Otmani <clarisseotm@gmail.com>
2024-10-17 16:05:24 +0200

    Modification du type de l'id

--- db/dump/1_db-creation.sql
+++ db/dump/1_db-creation.sql
@@ -2,18 +2,19 @@ DROP TABLE if exists public.account CASCADE;
 
 /* TODO : ajouter la création de la table "client" */
 
- CREATE TABLE client
- (
-id           SERIAL PRIMARY KEY,
-    first_name   VARCHAR(100),
-    last_name    VARCHAR(50),
-    email        VARCHAR(50),
-    birthday     date,
- )
+CREATE TABLE client
+(
+    id UUID PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name  VARCHAR(50),
+    email      VARCHAR(50),
+    birthdate   date
+);
 
 CREATE TABLE account
 (
-
+    id           SERIAL PRIMARY KEY,
     creationTime timestamp,
-    balance      bigint
+    balance      bigint,
+    id_client UUID references client (id)
 );
